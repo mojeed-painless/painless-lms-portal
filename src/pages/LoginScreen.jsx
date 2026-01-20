7b54e8c1686b5ad0cbb79851e96d5ad38392e133
mojeed-painless <shittumjd00@gmail.com>
2026-01-20 10:56:27 +0100

    UPDATED

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -115,15 +115,7 @@ const LoginScreen = () => {
               className={`auth-submit-btn ${isLoading ? 'disabled' : ''}`}
             >
               {isLoading ? (
-                <svg 
-                  className="auth-spinner" 
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                  focusable="false"
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <path  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
+                <div className="auth-spinner"></div>
               ) : (
                 'Login'
               )}
